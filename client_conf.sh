38a172a27fa3ac3aebfcf37987edb2827a515ab9
Wei Guangcheng <twksos@qq.com>
2016-04-21 19:39:30 -0700

    add client script

---
+++ client_conf.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ip addr add 10.0.3.15/24 dev toto0 
+ifconfig toto0 up
+route add -net 10.0.3.0 netmask 255.255.255.0 dev toto0
+route add -net 192.168.16.0 netmask 255.255.255.0 dev toto0
+sysctl -w net.ipv4.ip_forward=1
