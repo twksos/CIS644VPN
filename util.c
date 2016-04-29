3fa34d8af37c5be7ea2c54d5b8fae5e1b8f0166b
Wei Guangcheng <twksos@qq.com>
2016-04-28 22:00:51 -0400

    add str to hex and hex to str in util

--- util.c
+++ util.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void hex(const char *s, int bytes){
 	int i;
@@ -7,3 +8,38 @@ void hex(const char *s, int bytes){
 	}
 	printf("\n");
 }
+
+char * str_to_hex(char *src, int src_len){
+	int i;
+	char *hex = malloc((size_t) (src_len * 2 +1));
+	for(i=0;i<src_len;i++) {
+		sprintf(hex+i*2, "%02x", (unsigned char) src[i]);
+	}
+	hex[src_len * 2] = '\0';
+	return hex;
+}
+
+int hex_to_str(char * hex, int hex_len, char * str){
+	int i;
+	int tmp;
+	for(i=0;i<hex_len;i++) {
+		tmp = hex[i*2] - '0';
+		tmp = tmp << 4;
+		tmp += hex [i*2 + 1];
+		str[i] = (char) tmp;
+	}
+	return i>>1;
+}
+//
+//int main(){
+//	char * hex = str_to_hex("1234", 4);
+//	printf("hex: %s", hex);
+//	char * str = malloc(4);
+//	int strlen = hex_to_str(hex, 8, str);
+//	int i;
+//	printf("len %d", strlen);
+//
+//	for (i = 0; i < strlen; ++i) {
+//		printf("%c", str[i]);
+//	}
+//}
--- util.h
+++ util.h
@@ -2,5 +2,7 @@
 #define MY_UTIL_H_
 
 void hex(const char *s, int bytes);
+char * str_to_hex(char *src, int src_len);
+int hex_to_str(char * hex, int hex_len, char * str);
 
 #endif
